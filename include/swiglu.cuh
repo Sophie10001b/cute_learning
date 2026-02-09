9f6e6662635355cdce43573210dea5461af7e6db
Sophie10001b <Hhz029@hotmail.com>
2026-02-09 20:14:48 +0800

    modified:   .gitignore 	modified:   include/swiglu.cuh 	modified:   python/01_swiglu/cuda_jit.py

--- include/swiglu.cuh
+++ include/swiglu.cuh
@@ -16,27 +16,124 @@ struct SwiGLUParams {
     const void* up;
     const void* gate;
     void* output;
-    uint32_t M;
+    uint64_t M;
 };
 
 constexpr uint32_t kNumWarps = 4;
 constexpr uint32_t kThreadsPerBlock = kNumWarps * device::kWarpThreads;
 
+template <typename DType>
+__device__ DType swiglu(DType a, DType b) {
+    if constexpr (std::is_same_v<DType, fp32_t>) {
+        return a * (b * (1.0f / (1.0f + __expf(-b))));
+    }
+    else if constexpr (std::is_same_v<DType, fp16_t>) {
+        float a_f = __half2float(a);
+        float b_f = __half2float(b);
+        return __float2half(a_f * (b_f * (1.0f / (1.0f + __expf(-b_f)))));
+    }
+    else if constexpr (std::is_same_v<DType, bf16_t>) {
+        float a_f = __bfloat162float(a);
+        float b_f = __bfloat162float(b);
+        return __float2bfloat16(a_f * (b_f * (1.0f / (1.0f + __expf(-b_f)))));
+    }
+    else {
+        return a * (b * (1.0 / (1.0 + exp(-b))));
+    }
+};
+
 template <uint32_t kRowPerCTA, uint32_t kColPerCTA, int64_t kHiddenSize, bool kUsePDL, typename DType>
-__global__ void __launch_bounds__(128) swiglu_kernel(const __grid_constant__ SwiGLUParams params) {
+__global__ void swiglu_kernel(const __grid_constant__ SwiGLUParams params) {
     using namespace cute;
     const uint32_t tidx = threadIdx.x;
     const uint32_t bidx = blockIdx.x;
+    const uint32_t dimx = gridDim.x;
 
     const auto& [up, gate, output, M] = params;
     constexpr int64_t K = kHiddenSize;
+    constexpr uint32_t vec_width = 16 / sizeof(DType);
+    bool is_oob = (dimx * kRowPerCTA != M) && (bidx == dimx - 1);
 
     // 1. cute make layout
-    const auto gA_layout = make_ordered_layout(make_shape(_, Int<K>{}), make_step(_1{}, _0{}));
+    const auto gA_layout = make_ordered_layout(make_shape(M, Int<K>{}), make_step(_1{}, _0{}));
 
-    Tensor gA = make_tensor(make_gmem_ptr(reinterpret_cast<DType*>(up)), gA_layout);
-    Tensor gB = make_tensor(make_gmem_ptr(reinterpret_cast<DType*>(gate)), gA_layout);
-    Tensor gC = make_tensor(make_gmem_ptr(reinterpret_cast<DType*>(output)), gA_layout);
+    Tensor gA = make_tensor(make_gmem_ptr(static_cast<const DType*>(up)), gA_layout);
+    Tensor gB = make_tensor(make_gmem_ptr(static_cast<const DType*>(gate)), gA_layout);
+    Tensor gC = make_tensor(make_gmem_ptr(static_cast<DType*>(output)), gA_layout);
+
+    // 2. tv layout
+    auto thr_layout = make_ordered_layout(make_shape(Int<kRowPerCTA>{}, Int<kColPerCTA>{}), make_step(_1{}, _0{}));
+    auto val_layout = make_ordered_layout(make_shape(_1{}, Int<vec_width>{}), make_step(_1{}, _0{}));
+
+    // 3. copy atom
+    using copy_pred = Copy_Atom<Copy_Traits<UniversalCopy<uint_bit_t<sizeof(DType) * 8>>>, DType>;
+    using copy_vec = Copy_Atom<Copy_Traits<AutoVectorizingCopyWithAssumedAlignment<128>>, DType>;
+    
+    TiledCopy tiled_copy = make_tiled_copy(copy_pred{}, thr_layout, val_layout);
+    ThrCopy thr_copy = tiled_copy.get_slice(tidx);
+    auto tiler = product_each(shape(raked_product(thr_layout, val_layout)));
+    
+    // 4. tiling gmem
+    auto gA_tiled = zipped_divide(gA, tiler);
+    auto gB_tiled = zipped_divide(gB, tiler);
+    auto gC_tiled = zipped_divide(gC, tiler);
+
+    auto gCp = make_identity_tensor(shape(gC));
+    auto gCp_tiled = zipped_divide(gCp, tiler);
+
+    auto gAtA_0 = gA_tiled(make_coord(_, _), make_coord(bidx, 0));
+    auto gArA_0 = thr_copy.partition_S(gAtA_0);
+
+    Tensor rA = make_fragment_like(gArA_0);
+    Tensor rB = make_fragment_like(gArA_0);
+    Tensor rC = make_fragment_like(gArA_0);
+    Tensor rCp = make_tensor<bool>(shape(rC), stride(rC));
+
+    // 5. partition
+    constexpr uint32_t k_range = shape<1,1>(gA_tiled);
+    #pragma unroll
+    for (uint32_t i=0; i < k_range; ++i) {
+        auto gAtA = gA_tiled(make_coord(_, _), make_coord(bidx, i));
+        auto gBtB = gB_tiled(make_coord(_, _), make_coord(bidx, i));
+        auto gCtC = gC_tiled(make_coord(_, _), make_coord(bidx, i));
+        auto gCptCp = gCp_tiled(make_coord(_, _), make_coord(bidx, i));
+
+        auto gArA = thr_copy.partition_S(gAtA);
+        auto gBrB = thr_copy.partition_S(gBtB);
+        auto gCrC = thr_copy.partition_D(gCtC);
+        auto gCprCp = thr_copy.partition_S(gCptCp);
+
+        if (is_oob) {
+            #pragma unroll
+            for (uint32_t j=0; j < size(gCprCp); ++j) {
+                rCp(j) = elem_less(gCprCp(j), shape(gA));
+            }
+        }
+
+        if (is_oob) {
+            copy_if(copy_pred{}, rCp, gArA, rA);
+            copy_if(copy_pred{}, rCp, gBrB, rB);
+        }
+        else {
+            copy(copy_vec{}, gArA, rA);
+            copy(copy_vec{}, gBrB, rB);
+        }
+        
+        #pragma unroll
+        for (uint32_t j=0; j < size(rA); ++j) {
+            DType a = rA(j);
+            DType b = rB(j);
+            rC(j) = swiglu<DType>(a, b);
+        }
+
+        if (is_oob) {
+            copy_if(copy_pred{}, rCp, rC, gCrC);
+        }
+        else {
+            copy(copy_vec{}, rC, gCrC);
+        }
+
+    }
 
 }
 
@@ -46,7 +143,7 @@ struct SwiGLUKernel {
     static void run(
         const tvm::ffi::TensorView up,
         const tvm::ffi::TensorView gate,
-        const tvm::ffi::TensorView output,
+        const tvm::ffi::TensorView output
     ) {
         using namespace host;
         auto M = SymbolicSize{"num_tokens"};
@@ -54,6 +151,8 @@ struct SwiGLUKernel {
         auto device = SymbolicDevice{};
         K.set_value(kHiddenSize);
         device.set_options<kDLCUDA>();
+
+        RuntimeCheck(kHiddenSize % 128 == 0, "Hidden size must be divisible by 128");
 
         // checking on host
         TensorMatcher({M, K}) //
@@ -63,7 +162,7 @@ struct SwiGLUKernel {
             .verify(up).verify(gate).verify(output);
         
         static const uint32_t kNumSM = runtime::get_sm_count(device.unwrap().device_id);
-        uint32_t row_per_cta = 8;
+        uint32_t row_per_cta = 4;
         const auto num_tokens = static_cast<uint32_t>(M.unwrap());
         while ((num_tokens / row_per_cta) < kNumSM * 2 && row_per_cta > 1) row_per_cta >>= 1;
         
@@ -73,17 +172,17 @@ struct SwiGLUKernel {
             .output = output.data_ptr(),
             .M = num_tokens,
         };
-
-        constexpr uint32_t col_per_cta = 16 / sizeof(DType);
         
-        auto kernel = swiglu_kernel<1, col_per_cta, kHiddenSize, kUsePDL, DType>;
+        auto kernel = swiglu_kernel<1, kThreadsPerBlock, kHiddenSize, kUsePDL, DType>;
         switch (row_per_cta) {
-            case 8:
-                kernel = swiglu_kernel<8, col_per_cta, kHiddenSize, kUsePDL, DType>; break;
             case 4:
-                kernel = swiglu_kernel<4, col_per_cta, kHiddenSize, kUsePDL, DType>; break;
+                kernel = swiglu_kernel<4, kThreadsPerBlock / 4, kHiddenSize, kUsePDL, DType>; break;
             case 2:
-                kernel = swiglu_kernel<2, col_per_cta, kHiddenSize, kUsePDL, DType>; break;
+                kernel = swiglu_kernel<2, kThreadsPerBlock / 2, kHiddenSize, kUsePDL, DType>; break;
+            case 1:
+                kernel = swiglu_kernel<1, kThreadsPerBlock, kHiddenSize, kUsePDL, DType>; break;
+            default:
+                throw std::runtime_error("row_per_cta must be 1, 2, 4, 8");
         }
 
         LaunchKernel(div_ceil(num_tokens, row_per_cta), kThreadsPerBlock, device.unwrap())
--- python/01_swiglu/cuda_jit.py
+++ python/01_swiglu/cuda_jit.py
@@ -23,38 +23,79 @@ if TYPE_CHECKING:
     from tvm_ffi.module import Module
 
 @cache_once
-def _jit_rmsnorm_module(hidden_size: int, dtype: torch.dtype) -> Module:
+def _jit_swiglu_module(hidden_size: int, dtype: torch.dtype) -> Module:
     args = make_cpp_args(hidden_size, is_arch_support_pdl(), dtype)
     return load_jit(
-        "rmsnorm",
+        "swiglu",
         *args,
-        cuda_files=[str(ROOT_PATH / "include" / "rmsnorm.cuh")],
-        cuda_wrappers=[("rmsnorm", f"RMSNormKernel<{args}>::run")],
+        cuda_files=[str(ROOT_PATH / "include" / "swiglu.cuh")],
+        cuda_wrappers=[("swiglu", f"SwiGLUKernel<{args}>::run")],
         extra_cflags=DEFAULT_CFLAGS,
         extra_cuda_cflags=DEFAULT_CUDA_CFLAGS,
         extra_include_paths=THIRD_PARTY_HEADER_DIRS,
     )
 
 
-def rmsnorm(
-    input: torch.Tensor,
-    weight: torch.Tensor,
-    output: torch.Tensor,
-    eps: float = 1e-6,
-) -> None:
-    module = _jit_rmsnorm_module(input.shape[-1], input.dtype)
-    module.rmsnorm(input, weight, output, eps)
+def swiglu(
+    up: torch.Tensor,
+    gate: torch.Tensor,
+    output: Optional[torch.Tensor]=None,
+) -> torch.Tensor:
+    if output is None:
+        output = torch.empty_like(up)
+    
+    module = _jit_swiglu_module(up.shape[-1], up.dtype)
+    module.swiglu(up, gate, output)
+    return output
 
-def test():
-    a = torch.randn(512, 1024, dtype=torch.float16, device='cuda')
-    b = torch.randn(1024, dtype=torch.float16, device='cuda')
-    c = torch.empty_like(a)
-    eps = 1e-6
+def swiglu_torch(
+    up: torch.Tensor,
+    gate: torch.Tensor,
+) -> torch.Tensor:
+    return up * (gate * torch.sigmoid(gate))
 
-    ref = torch.nn.functional.rms_norm(a, [a.shape[-1]], b, eps)
-    rmsnorm(a, b, c, eps)
+@torch.compile
+def swiglu_torch_compile(
+    up: torch.Tensor,
+    gate: torch.Tensor,
+) -> torch.Tensor:
+    return up * (gate * torch.sigmoid(gate))
 
-    assert torch.allclose(c, ref, atol=1e-3, rtol=1e-3)
+
+def test(m: int, k: int):
+    up = torch.randn(m, k, dtype=torch.float16, device='cuda')
+    gate = torch.randn(m, k, dtype=torch.float16, device='cuda')
+
+    ref = up * (gate * torch.sigmoid(gate))
+    output = swiglu(up, gate)
+
+    assert torch.allclose(output, ref, atol=1e-2, rtol=1e-2)
+    print(f"✅ pass swiglu in [M:{m}, K:{k}], max diff: {torch.max(torch.abs(ref - output))}")
+
+def trace(m: int, k: int):
+    up = torch.randn(m, k, device="cuda")
+    gate = torch.randn(m, k, device="cuda")
+
+    profiler = torch.profiler.profile(
+        activities=[torch.profiler.ProfilerActivity.CPU, torch.profiler.ProfilerActivity.CUDA],
+        schedule=torch.profiler.schedule(wait=1, warmup=1, active=3, repeat=1),
+        on_trace_ready=torch.profiler.tensorboard_trace_handler('./profiler_logs', use_gzip=True),
+        record_shapes=True,
+        profile_memory=True,
+        with_stack=True,
+    )
+
+    profiler.start()
+
+    for i in range(10):
+        ref1 = swiglu_torch(up, gate)
+        ref2 = swiglu_torch_compile(up, gate)
+        output = swiglu(up, gate)
+        profiler.step()
+
+    profiler.stop()
+
 
 if __name__ == "__main__":
-    test()
+    trace(8192, 8192)
+    # test(8111, 8192)
