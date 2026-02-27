3ddd99bf24a5770cbe0e64ed6317215df0d67753
Sophie10001b <Hhz029@hotmail.com>
2026-02-27 20:08:27 +0800

    update

--- include/gather_scatter_gemm_sm80.cuh
+++ include/gather_scatter_gemm_sm80.cuh
@@ -21,7 +21,13 @@ namespace {
 #define SMEM_SIZE 102400
 #endif
 
-__forceinline__ uint32_t next_pow_of_2(uint32_t x) {
+template <typename DType, class LayoutA, class LayoutB>
+struct SharedMemory {
+    cute::ArrayEngine<DType, cute::cosize_v<LayoutA>> A;
+    cute::ArrayEngine<DType, cute::cosize_v<LayoutB>> B;
+};
+
+constexpr uint32_t next_pow_of_2(uint32_t x) {
     if (x <= 0) return 0;
     return 1 << (32 - __builtin_clz(x));
 }
@@ -45,84 +51,171 @@ struct GEMMParams {
 constexpr uint32_t kNumWarps = 4;
 constexpr uint32_t kThreadsPerBlock = kNumWarps * device::kWarpThreads;
 
-template <uint32_t kBM, uint32_t kBN, uint32_t kBK, uint32_t kPipeline,
+template <uint32_t kBM, uint32_t kBN, uint32_t kBK, uint32_t kPipeline, class SLayoutA, class SLayoutB,
           uint32_t kHiddenSize, uint32_t kIntermediateSize, uint32_t kGroupSize, uint32_t kNumGroup, uint32_t kGroupIter,
-          uint32_t kRowG2SPerCTA, bool kUsePDL, typename DType, ActivationType kActivation>
+          uint32_t kRowG2SPerCTA, bool kUsePDL, typename DType, uint32_t kActivation>
 __global__ void gather_scatter_gemm_kernel(const __grid_constant__ GEMMParams params) {
+    using namespace cute;
+    const uint32_t tidx = threadIdx.x;
+    const uint32_t bidx = blockIdx.x;
+    const uint32_t bidy = blockIdx.y;
+    const uint32_t bidz = blockIdx.z;
+    const uint32_t bdimx = gridDim.x;
+    const uint32_t warp_id = tidx / device::kWarpThreads;
+    const uint32_t lane_id = tidx % device::kWarpThreads;
 
+    constexpr auto BM = kBM;
+    constexpr auto BN = kBN;
+    constexpr auto BK = kBK;
+    constexpr auto Pipeline = kPipeline;
+    constexpr auto N = kIntermediateSize;
+    constexpr auto K = kHiddenSize;
+    constexpr auto G = kGroupSize;
+    constexpr auto NG = kNumGroup;
+    constexpr auto GI = kGroupIter;
+    constexpr auto RowG2SPerCTA = kRowG2SPerCTA;
+    const auto& [A, B, Mask, Index, C, M] = params;
+    constexpr uint32_t vec_width = 128 / sizeof(DType);
+
+    //
+    // prepare tensors and smem layout
+    //
+
+    Tensor mA = make_tensor(
+        make_gmem_ptr(static_cast<const DType*>(A)),
+        make_shape(M, Int<K>{}),
+        make_stride(Int<K>{}, _1{})
+    );
+    Tensor mB = make_tensor(
+        make_gmem_ptr(static_cast<const DType*>(B)),
+        make_shape(N, Int<K>{}),
+        make_stride(Int<K>{}, _1{})
+    );
+    Tensor mC = make_tensor(
+        make_gmem_ptr(static_cast<DType*>(C)),
+        make_shape(M, Int<N>{}),
+        make_stride(Int<N>{}, _1{})
+    );
+    Tensor mMask = make_tensor(
+        make_gmem_ptr(static_cast<const uint8_t*>(Mask)),
+        make_shape(Int<NG>{}, M),
+        make_stride(M, _1{})
+    );
+    Tensor mIndex = make_tensor(
+        make_gmem_ptr(static_cast<const uint32_t*>(Index)),
+        make_shape(Int<NG>{}, M),
+        make_stride(M, _1{})
+    );
+
+    extern __shared__ uint8_t shared_memory[];
+    using SharedMemory = SharedMemory<DType, SLayoutA, SLayoutB>;
+    SharedMemory &smem = *reinterpret_cast<SharedMemory*>(shared_memory);
+    Tensor sA = make_tensor(make_smem_ptr(smem.A.begin()), make_shape(Int<BM>{}, Int<BK>{}, Int<Pipeline>{}));
+    Tensor sB = make_tensor(make_smem_ptr(smem.B.begin()), make_shape(Int<BN>{}, Int<BK>{}, Int<Pipeline>{}));
+
+    //
+    // load mask & index
+    //
+    const uint32_t Mask_row_offset = bidx * BM;
+    constexpr uint32_t thr_per_col_Mask = kThreadsPerBlock / RowG2SPerCTA;
+    const uint32_t Mask_row = tidx / thr_per_col_Mask;
+    Tensor rMask = make_fragment_like(make_shape(Int<BM / RowG2SPerCTA>{}));
+    Tensor rMask_cta = make_fragment_like(make_shape(Int<BM / RowG2SPerCTA>{}));
+
+    #pragma unroll
+    for (uint32_t i=0, offset=Mask_row_offset + Mask_row; i < cosize(rMask); i++, offset += RowG2SPerCTA) {
+        rMask(i) = offset < M ? mMask(make_coord(bidy, offset)) : 0;
+        rMask_cta(i) = __syncthreads_or(rMask(i)) > 0;
+    }
 }
 
 
-template <uint32_t kHiddenSize, uint32_t kIntermediateSize, uint32_t kGroupSize, uint32_t kNumGroup, bool kUsePDL, typename DType, ActivationType kActivation>
+template <uint32_t kHiddenSize, uint32_t kIntermediateSize, uint32_t kGroupSize, uint32_t kNumGroup, bool kUsePDL, typename DType, uint32_t kActivation>
 struct GatherScatterGEMMKernelSM80 {
 
+    // template <uint32_t kBM, uint32_t kBN, uint32_t kBK, uint32_t kPipeline, uint32_t kGroupIter, uint32_t kRowG2SPerCTA>
+    // static constexpr auto impl = gather_scatter_gemm_kernel<kBM, kBN, kBK, kPipeline, kHiddenSize, kIntermediateSize, kGroupSize, kNumGroup, kGroupIter, kRowG2SPerCTA, kUsePDL, DType, kActivation>;
+
     template <uint32_t kBM, uint32_t kBN, uint32_t kBK, uint32_t kPipeline, uint32_t kGroupIter, uint32_t kRowG2SPerCTA>
-    static constexpr auto impl = gather_scatter_gemm_kernel<kBM, kBN, kBK, kPipeline, kHiddenSize, kIntermediateSize, kGroupSize, kNumGroup, kGroupIter, kRowG2SPerCTA, kUsePDL, DType, kActivation>;
+    static auto impl(const GEMMParams &params, const host::SymbolicDevice &device) {
+        // smem layout dispatch
+        using namespace cute;
+        using SLayoutAAtom = decltype(GMMA::Layout_K_SW128_Atom<DType>{});
+        using SLayoutBAtom = decltype(GMMA::Layout_K_SW128_Atom<DType>{});
+        using SLayoutA = decltype(tile_to_shape(SLayoutAAtom{}, make_shape(Int<kBM>{}, Int<kBK>{}, Int<kPipeline>{})));
+        using SLayoutB = decltype(tile_to_shape(SLayoutBAtom{}, make_shape(Int<kBN>{}, Int<kBK>{}, Int<kPipeline>{})));
+
+        static constexpr size_t smem_size = size_t(sizeof(SharedMemory<DType, SLayoutA, SLayoutB>));
+        printf("Dynamic SMEM Size: %lu\n", smem_size);
+
+        constexpr auto kernel = gather_scatter_gemm_kernel<kBM, kBN, kBK, kPipeline, SLayoutA, SLayoutB, kHiddenSize, kIntermediateSize, kGroupSize, kNumGroup, kGroupIter, kRowG2SPerCTA, kUsePDL, DType, kActivation>;
+
+        const dim3 grid_size = {cute::ceil_div(params.M, kBM), kNumGroup, kGroupIter};
+        const dim3 block_size = {kThreadsPerBlock, 1, 1};
+        host::LaunchKernel(grid_size, block_size, device.unwrap(), smem_size).enable_pdl(kUsePDL)(kernel, params);
+    }
 
     template <uint32_t kBN, uint32_t kGroupIter>
-    static auto dispatch_BM(uint32_t BM, uint32_t BK, uint32_t pipeline) {
+    static auto dispatch_BM(uint32_t BM, uint32_t BK, uint32_t pipeline, const GEMMParams &params, const host::SymbolicDevice &device) {
         switch (BM) {
-            case 16: return dispatch_BK<16, kBN, kGroupIter>(BK, pipeline);
-            case 32: return dispatch_BK<32, kBN, kGroupIter>(BK, pipeline);
-            case 64: return dispatch_BK<64, kBN, kGroupIter>(BK, pipeline);
-            case 128: return dispatch_BK<128, kBN, kGroupIter>(BK, pipeline);
-            default: return dispatch_BK<16, kBN, kGroupIter>(BK, pipeline);
+            case 16: return dispatch_BK<16, kBN, kGroupIter>(BK, pipeline, params, device);
+            case 32: return dispatch_BK<32, kBN, kGroupIter>(BK, pipeline, params, device);
+            case 64: return dispatch_BK<64, kBN, kGroupIter>(BK, pipeline, params, device);
+            case 128: return dispatch_BK<128, kBN, kGroupIter>(BK, pipeline, params, device);
+            default: return dispatch_BK<16, kBN, kGroupIter>(BK, pipeline, params, device);
         }
     }
 
     template <uint32_t kBM, uint32_t kBN, uint32_t kGroupIter>
-    static auto dispatch_BK(uint32_t BK, uint32_t pipeline) {
+    static auto dispatch_BK(uint32_t BK, uint32_t pipeline, const GEMMParams &params, const host::SymbolicDevice &device) {
         switch (BK) {
-            case 64: return dispatch_pipeline<kBM, kBN, 64, kGroupIter, kThreadsPerBlock / (64 / 8)>(pipeline);
-            case 128: return dispatch_pipeline<kBM, kBN, 128, kGroupIter, kThreadsPerBlock / (128 / 8)>(pipeline);
-            default: return dispatch_pipeline<kBM, kBN, 64, kGroupIter, kThreadsPerBlock / (64 / 8)>(pipeline);
+            case 64: return dispatch_pipeline<kBM, kBN, 64, kGroupIter, kThreadsPerBlock / (64 / 8)>(pipeline, params, device);
+            case 128: return dispatch_pipeline<kBM, kBN, 128, kGroupIter, kThreadsPerBlock / (128 / 8)>(pipeline, params, device);
+            default: return dispatch_pipeline<kBM, kBN, 64, kGroupIter, kThreadsPerBlock / (64 / 8)>(pipeline, params, device);
         }
     }
 
     template <uint32_t kBM, uint32_t kBN, uint32_t kBK, uint32_t kGroupIter, uint32_t kRowG2SPerCTA>
-    static auto dispatch_pipeline(uint32_t pipeline) {
+    static auto dispatch_pipeline(uint32_t pipeline, const GEMMParams &params, const host::SymbolicDevice &device) {
         switch (pipeline) {
-            case 2: return impl<kBM, kBN, kBK, 2, kGroupIter, kRowG2SPerCTA>;
-            case 3: return impl<kBM, kBN, kBK, 3, kGroupIter, kRowG2SPerCTA>;
-            case 4: return impl<kBM, kBN, kBK, 4, kGroupIter, kRowG2SPerCTA>;
-            default: return impl<kBM, kBN, kBK, 3, kGroupIter, kRowG2SPerCTA>;
+            case 2: return impl<kBM, kBN, kBK, 2, kGroupIter, kRowG2SPerCTA>(params, device);
+            case 3: return impl<kBM, kBN, kBK, 3, kGroupIter, kRowG2SPerCTA>(params, device);
+            case 4: return impl<kBM, kBN, kBK, 4, kGroupIter, kRowG2SPerCTA>(params, device);
+            default: return impl<kBM, kBN, kBK, 3, kGroupIter, kRowG2SPerCTA>(params, device);
         }
     }
 
-    static auto kernel_dispatch(uint32_t M, float sparsity) {
+    static void kernel_dispatch(uint32_t M, float sparsity, const GEMMParams &params, const host::SymbolicDevice &device) {
         using namespace host;
         constexpr uint32_t N = kIntermediateSize;
         constexpr uint32_t K = kHiddenSize;
         constexpr uint32_t G = kGroupSize;
 
-        uint32_t pipiline = 3;
+        uint32_t pipeline = 2;
         uint32_t BM = next_pow_of_2(uint32_t(M * sparsity));
         if (BM > M) BM >>= 1;
         BM = std::min(uint32_t(128), std::max(uint32_t(16), BM));
 
         RuntimeCheck(
-            std::has_single_bit(kGroupSize) && kGroupSize >= 16,
+            std::has_single_bit(G) && G >= 16,
             "kGroupSize must be power of 2 and >= 16"
         );
-        constexpr uint32_t BN = std::min(uint32_t(128), std::max(uint32_t(16), next_pow_of_2(kGroupSize)));
-        constexpr uint32_t GroupIter = kGroupSize / BN;
+        constexpr uint32_t BN = std::min(uint32_t(128), std::max(uint32_t(16), next_pow_of_2(G)));
+        constexpr uint32_t GroupIter = G / BN;
 
         uint32_t BK = 128;
-        while ((BM * BK + BN * BK) * pipiline > (SMEM_SIZE / 2) && BK > 64) BK >>= 1;
+        while ((BM * BK + BN * BK) * pipeline * sizeof(DType) > (SMEM_SIZE / 2) && BK > 64) BK >>= 1;
+        while ((BM * BK + BN * BK) * pipeline * sizeof(DType) > (SMEM_SIZE / 2) && BM > 16) BM >>= 1;
         
         RuntimeCheck(
-            (BM * BK + BN * BK) * pipiline <= SMEM_SIZE,
-            "BM * BK + BN * BK * pipiline must be less than or equal to SMEM_SIZE"
+            (BM * BK + BN * BK) * pipeline <= SMEM_SIZE,
+            "BM * BK + BN * BK * pipeline must be less than or equal to SMEM_SIZE"
         );
 
-        // dispatch
         const uint32_t kRowG2SPerCTA = kThreadsPerBlock / (BK / 8);
-        auto kernel = dispatch_BM<BN, GroupIter>(BM, BK, pipiline);
 
-        const dim3 grid_size = {cute::ceil_div(M, BM), kNumGroup, GroupIter};
-        const dim3 block_size = {kThreadsPerBlock, 1, 1};
-
-        return std::tuple(kernel, grid_size, block_size);
+        // printf("BM: %d, BN: %d, BK: %d, pipeline: %d, GroupIter: %d, kRowG2SPerCTA: %d\n", BM, BN, BK, pipeline, GroupIter, kRowG2SPerCTA);
+        dispatch_BM<BN, GroupIter>(BM, BK, pipeline, params, device);
     }
 
     static void run(
@@ -179,7 +272,7 @@ struct GatherScatterGEMMKernelSM80 {
         
         TensorMatcher({NG, M}) //
             .with_strides({M, 1})
-            .with_dtype<uint8_t>()
+            .with_dtype<uint32_t>()
             .with_device(device)
             .verify(Index);
         
@@ -204,10 +297,7 @@ struct GatherScatterGEMMKernelSM80 {
             .C = C.data_ptr(),
             .M = num_tokens
         };
-
-        auto [kernel, grid_size, block_size] = kernel_dispatch(num_tokens, sparsity);
-        LaunchKernel(grid_size, block_size, device.unwrap())
-            .enable_pdl(kUsePDL)(kernel, params);
+        kernel_dispatch(num_tokens, sparsity, params, device);
     }
 };
 
---
+++ python/03_gather_scatter_gemm/cuda_jit.py
@@ -0,0 +1,89 @@
+from __future__ import annotations
+
+import logging
+from typing import TYPE_CHECKING, Optional
+
+import torch
+
+from sglang.jit_kernel.utils import (
+    cache_once,
+    is_arch_support_pdl,
+    load_jit,
+    make_cpp_args,
+)
+
+from utils import (
+    ROOT_PATH,
+    THIRD_PARTY_HEADER_DIRS,
+    DEFAULT_CFLAGS,
+    DEFAULT_CUDA_CFLAGS,
+)
+
+if TYPE_CHECKING:
+    from tvm_ffi.module import Module
+
+ACTIVATION = {
+    "Identity": 0,
+    "ReLU": 1,
+    "SiLU": 2,
+}
+
+@cache_once
+def _jit_gather_scatter_gemm_module(
+    hidden_size: int,
+    intermediate_size: int,
+    group_size: int,
+    num_group: int,
+    dtype: torch.dtype,
+    activation: str,
+) -> Module:
+    args = make_cpp_args(
+        hidden_size, intermediate_size, group_size, num_group, is_arch_support_pdl(), dtype, ACTIVATION.get(activation, 0)
+    )
+    return load_jit(
+        "gather_scatter_gemm_sm80",
+        *args,
+        cuda_files=[str(ROOT_PATH / "include" / "gather_scatter_gemm_sm80.cuh")],
+        cuda_wrappers=[("gather_scatter_gemm_sm80", f"GatherScatterGEMMKernelSM80<{args}>::run")],
+        extra_cflags=DEFAULT_CFLAGS,
+        extra_cuda_cflags=DEFAULT_CUDA_CFLAGS,
+        extra_include_paths=THIRD_PARTY_HEADER_DIRS,
+    )
+
+
+def gather_scatter_gemm(
+    A: torch.Tensor,
+    B: torch.Tensor,
+    Mask: torch.Tensor,
+    C: Optional[torch.Tensor]=None,
+    activation: Optional[str]="Identity",
+    estimate_sparsity: Optional[float]=0.5,
+) -> torch.Tensor:
+    assert A.dim() == 3 and B.dim() == 2
+    bsz, seqlen, K = A.shape
+    A_F = A.flatten(0, 1)
+
+    M, K = A_F.shape
+    N, _ = B.shape
+    if C is None:
+        C = torch.zeros((M, N), dtype=A.dtype, device=A.device)
+    
+    if Mask.dim() == 2: Mask = Mask.unsqueeze(0)
+    Mask_T = Mask.flatten(0, 1).transpose(0, 1).contiguous()
+    Mask_ST, Index = torch.sort(Mask_T, dim=-1, descending=True, stable=False)
+
+    NG = Mask.shape[-1]
+    G = K // NG
+
+    module = _jit_gather_scatter_gemm_module(K, N, G, NG, A.dtype, activation)
+    module.gather_scatter_gemm_sm80(A_F, B, Mask_ST.to(torch.uint8), Index.to(torch.uint32), C, estimate_sparsity)
+    return C.reshape(bsz, seqlen, N)
+
+def test(bsz: int, seqlen: int, N: int, K: int, G: int, sparsity: float):
+    A = torch.randn((bsz, seqlen, K), dtype=torch.float16, device="cuda")
+    B = torch.randn((N, K), dtype=torch.float16, device="cuda")
+    Mask = torch.rand((bsz, seqlen, N // G), device="cuda") > sparsity
+    C = gather_scatter_gemm(A, B, Mask, activation="SiLU", estimate_sparsity=sparsity)
+
+if __name__ == "__main__":
+    test(1, 1024, 4096, 4096, 128, 0.5)
